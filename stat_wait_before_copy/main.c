7a4b600400c11647998041430395173527861dc3
LiXiaoyu <hittingnote@gmail.com>
2025-07-06 02:11:36 +0800

    impl_stat_wait_before_copy

--- stat_wait_before_copy/main.c
+++ stat_wait_before_copy/main.c
@@ -14,6 +14,67 @@ static inline double get_timestamp(char *strln) {
 	return ret;
 }
 
+struct pid_to_range_node {
+	pid_t						pid;
+	double						start;
+	double						end;
+	struct rb_node				node;
+};
+
+static declare_and_init_rbtree(pid_to_range_tree);
+
+static struct pid_to_range_node *to_pid_range_node(struct rb_node *n) {
+	return n? container_of(n, struct pid_to_range_node, node): NULL;
+}
+
+static int pid_to_range_node_compare(const struct rb_node *n1, const struct rb_node *n2) {
+	struct pid_to_range_node *ent1 = to_pid_range_node(n1);
+	struct pid_to_range_node *ent2 = to_pid_range_node(n2);
+
+	if(ent1->pid < ent2->pid) {
+		return -1;
+	}
+	else if(ent1->pid > ent2->pid) {
+		return 1;
+	}
+	else
+		return 0;
+}
+
+static struct pid_to_range_node *search_pid_range_node(pid_t pid,
+						struct rb_node **p_parent, struct rb_node ***p_insert) {
+	struct pid_to_range_node target = {.pid = pid};
+	struct rb_node *match = ___search(&target.node, &pid_to_range_tree, p_parent, p_insert,
+							SEARCH_EXACTLY, pid_to_range_node_compare);
+	return to_pid_range_node(match);
+}
+
+static struct pid_to_range_node *add_pid_to_range_node(pid_t pid) {
+	struct pid_to_range_node *ent;
+	struct rb_node *parent, **insert;
+
+	ent = search_pid_range_node(pid, &parent, &insert);
+	if(ent) {
+		return NULL;
+	}
+
+	ent = malloc(sizeof(*ent));
+	if(!ent) {
+		return NULL;
+	}
+
+	ent->pid = pid;
+	rbtree_add_node(&ent->node, parent, insert, &pid_to_range_tree);
+	return ent;
+}
+
+static struct pid_to_range_node *get_pid_to_range_node(pid_t pid) {
+	struct pid_to_range_node *ent;
+
+	ent = search_pid_range_node(pid, &parent, &insert);
+	return ent;
+}
+
 int main(int argc, char *argv[]) {
 	int fd_dir;
 	DIR *fd_DIR;
@@ -79,6 +140,54 @@ int main(int argc, char *argv[]) {
 
 	end = get_timestamp(strln);
 	restore_total = end - start;
+
+	fseek(fp, 0, SEEK_SET);
+	while(fgets(strln, 32768, fp) != NULL) {
+		struct pid_to_range_node *node;
+		pid_t pid;
+		double start;
+
+		if(!strstr(strln, "metadata")) {
+			continue;
+		}
+
+		sscanf(strln, "%lf%d", &start, &pid);
+		if(pid == 1) {
+			continue;
+		}
+
+		node = add_pid_to_range_node(pid);
+		if(!node) {
+			fprintf(stderr, "Parse log error\n");
+			exit(-1);
+		}
+
+		node->start = start * 1000.0;
+	}
+
+	fseek(fp, 0, SEEK_SET);
+	while(fgets(strln, 32768, fp) != NULL) {
+		struct pid_to_range_node *node;
+		pid_t pid;
+		double end;
+
+		if(!strstr(strln, "Restore RDMA communication")) {
+			continue;
+		}
+
+		sscanf(strln, "%lf%d", &end, &pid);
+		if(pid == 1) {
+			continue;
+		}
+
+		node = get_pid_to_range_node(pid);
+		if(!node) {
+			fprintf(stderr, "Parse log error\n");
+			exit(-1);
+		}
+
+		node->end = end * 1000.0;
+	}
 
 	return 0;
 }
