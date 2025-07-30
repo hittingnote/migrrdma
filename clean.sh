489eb77b915e690e7ffad7aa7821da8fc0664d63
hittingnote <hittingnote@gmail.com>
2025-07-30 22:05:00 +0800

    build and clean script

---
+++ clean.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd src/mlnx-ofed-kernel-5.4/
+rm $(find . -name .gitignore)
+totl=`git status | wc -l`
+headl=`git status | grep Untracked -n | awk -F '[:]' '{print $1}'`
+rm $(git status | tail -n `expr ${totl} - ${headl} - 1` | grep -v "nothing added to commit" | grep -v "no changes added to commit") -r
+git restore $(git status | grep modified | grep -v "\\.\\." | awk '{print $2}')
+git restore $(git status | grep deleted | grep -v "\\.\\." | awk '{print $2}')
+
+cd ../rdma-core-54mlnx1/
+rm $(find . -name .gitignore)
+totl=`git status | wc -l`
+headl=`git status | grep Untracked -n | awk -F '[:]' '{print $1}'`
+rm $(git status | tail -n `expr ${totl} - ${headl} - 1` | grep -v "nothing added to commit" | grep -v "no changes added to commit") -r
+git restore $(git status | grep modified | grep -v "\\.\\." | awk '{print $2}')
+git restore $(git status | grep deleted | grep -v "\\.\\." | awk '{print $2}')
+
+cd ../criu-3.18/
+rm $(find . -name .gitignore)
+totl=`git status | wc -l`
+headl=`git status | grep Untracked -n | awk -F '[:]' '{print $1}'`
+rm $(git status | tail -n `expr ${totl} - ${headl} - 1` | grep -v "nothing added to commit" | grep -v "no changes added to commit") -r
+git restore $(git status | grep modified | grep -v "\\.\\." | awk '{print $2}')
+git restore $(git status | grep deleted | grep -v "\\.\\." | awk '{print $2}')
+
+cd ../runc/
+rm $(find . -name .gitignore)
+totl=`git status | wc -l`
+headl=`git status | grep Untracked -n | awk -F '[:]' '{print $1}'`
+rm $(git status | tail -n `expr ${totl} - ${headl} - 1` | grep -v "nothing added to commit" | grep -v "no changes added to commit") -r
+git restore $(git status | grep modified | grep -v "\\.\\." | awk '{print $2}')
+git restore $(git status | grep deleted | grep -v "\\.\\." | awk '{print $2}')
