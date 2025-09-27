94aa5bc517897a78352f05922efe690925bb91e8
hittingnote <hittingnote@gmail.com>
2025-09-27 22:18:31 +0800

    eval_basic.sh: Update

--- eval/eval_basic.sh
+++ eval/eval_basic.sh
@@ -10,13 +10,13 @@ shift 5
 
 cd ../
 
-ssh root@${partner} `pwd`/src/migrrdma_daemon/migrrdma_daemon ${ibdev} &> /dev/null &
+ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${partner} sudo `pwd`/src/migrrdma_daemon/migrrdma_daemon ${ibdev} &> /dev/null &
 
 if [ "${migr_who}" == "recv" ]; then
 	./rdma_migr_demo.sh ${pre_setup} ${migr_dst} $@ -d ${ibdev} --use_old_post_send --run_infinitely |
 				grep -E "DumpRDMA|DumpOthers|Transfer|RestoreRDMA|FullRestore" &
 	sleep 2
-	ssh root@${partner} $@ -d ${ibdev} --use_old_post_send --run_infinitely \
+	ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${partner} $@ -d ${ibdev} --use_old_post_send --run_infinitely \
 				$(show_gids | grep ${ibdev} | tail -n 1 | awk '{print $5}') 2> /dev/null |
 			       	while read LINE; do
 					if [ -n "`echo $LINE | grep -E \"GID|local|remote\"`" ]; then
@@ -25,7 +25,7 @@ if [ "${migr_who}" == "recv" ]; then
 					echo "[FROM Partner (Sender)]: $LINE" > /dev/stderr
 				done &
 elif [ "${migr_who}" == "send" ]; then
-	ssh root@${partner} $@ -d ${ibdev} --use_old_post_send --run_infinitely 2> /dev/null |
+	ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${partner} $@ -d ${ibdev} --use_old_post_send --run_infinitely 2> /dev/null |
 				while read LINE; do
 					if [ -n "`echo $LINE | grep -E \"GID|local|remote\"`" ]; then
 						continue
@@ -42,8 +42,7 @@ fi
 sleep 60
 
 docker rm -f test
-ssh root@${migr_dst} docker rm -f test1
-ssh root@${migr_dst} docker rm -f test1
-ssh root@${partner} pkill -9 `echo $@ | awk '{print $1}'`
-ssh root@${partner} pkill -9 migrrdma_daemon
-
+ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${migr_dst} sudo docker rm -f test1
+ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${migr_dst} sudo docker rm -f test1
+ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${partner} sudo pkill -9 `echo $@ | awk '{print $1}'`
+ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${partner} sudo pkill -9 migrrdma_daemon
