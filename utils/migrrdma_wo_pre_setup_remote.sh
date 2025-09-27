e22f95ebca6851beb01ed1a40658973320e136f4
hittingnote <hittingnote@gmail.com>
2025-09-27 22:17:37 +0800

    utils

--- utils/migrrdma_wo_pre_setup_remote.sh
+++ utils/migrrdma_wo_pre_setup_remote.sh
@@ -11,7 +11,7 @@ mkdir /dev/shm/dump /dev/shm/dumprdma /dev/shm/restorerdma/ /dev/shm/workpath
 
 #sleep 27
 
-ssh root@${migr_dst} `pwd`/utils/remote_start_cont.sh ${new_cont}
+ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${migr_dst} sudo `pwd`/utils/remote_start_cont.sh ${new_cont}
 orig_cont_id=`docker inspect ${orig_cont} | grep Id | awk -F '[" ]+' '{print $4}'`
 old_init_pid=`docker inspect ${orig_cont} | grep \"Pid\" | awk -F '[", ]+' '{print $4}'`
 
@@ -37,21 +37,22 @@ mkdir /dev/shm/restorerdma/fdiff
 cp `docker inspect ${orig_cont} | grep Upper | awk -F '[:," ]+' '{print $3}'`/* /dev/shm/restorerdma/fdiff/ -r
 
 start=`date +"%s.%N"`
-scp -q -r /dev/shm/restorerdma/ root@${migr_dst}:/dev/shm/
+chown ${SUDO_USER} /dev/shm/restorerdma/ -R
+scp -q -r -i `eval echo ~$SUDO_USER`/.ssh/id_rsa /dev/shm/restorerdma/ ${SUDO_USER}@${migr_dst}:/dev/shm/
 end=`date +"%s.%N"`
-ssh root@${migr_dst} `pwd`/utils/remote_restore.sh ${new_cont} ${migr_dst} `pwd`/utils/fullrestore/rdma_fullrestore
+ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${migr_dst} sudo `pwd`/utils/remote_restore.sh ${new_cont} ${migr_dst} `pwd`/utils/fullrestore/rdma_fullrestore
 
 mkdir /dev/shm/restorerdma/checkpoint1/
 
-while [ -z "`ssh root@${migr_dst} ls /dev/shm/restore_*.log 2> /dev/null`" ]; do
+while [ -z "`ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${migr_dst} ls /dev/shm/restore_*.log 2> /dev/null`" ]; do
 	sleep 1
 done
 
-while [ -n "`ssh root@${migr_dst} ps -C criu | grep -v PID`" ]; do
+while [ -n "`ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${migr_dst} ps -C criu | grep -v PID`" ]; do
 	sleep 1
 done
 
-scp -q -r root@${migr_dst}:/dev/shm/restore*.log /dev/shm/
+scp -q -r -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${migr_dst}:/dev/shm/restore*.log /dev/shm/
 
 transfer_time=`echo "scale=3; ($end - $start) * 1000.0" | bc`
 total_dump_raw=`cat /dev/shm/dump_*.log | tail -n 1 | awk -F '[()]+' '{print $2}'`
@@ -88,5 +89,5 @@ echo "RestoreRDMA: ${restore_rdma} ms"
 
 cd /dev/shm/
 rm *.json checkpoint_time *.log dump_img/ predump_img/ restorerdma/ workpath *.sock dump dumprdma -r
-ssh root@${migr_dst} rm /dev/shm/*.json /dev/shm/checkpoint_time /dev/shm/*.log /dev/shm/dump_img/ /dev/shm/predump_img/ \
+ssh -i `eval echo ~$SUDO_USER`/.ssh/id_rsa ${SUDO_USER}@${migr_dst} sudo rm /dev/shm/*.json /dev/shm/checkpoint_time /dev/shm/*.log /dev/shm/dump_img/ /dev/shm/predump_img/ \
 						/dev/shm/restorerdma/ /dev/shm/workpath /dev/shm/*.sock /dev/shm/dump /dev/shm/dumprdma -r
