23b3a5fa13a0c1816748b7e2e2950dec66f1ddb7
hittingnote <hittingnote@gmail.com>
2025-09-30 16:04:51 +0800

    Eval 02: Overhead of Wait-before-stop

--- eval/02_wbs_overhead/script.sh
+++ eval/02_wbs_overhead/script.sh
@@ -8,7 +8,7 @@ echo -e "#MsgSize\t\"WaitBeforeStop\"\tBlackout"
 
 for i in 512 32768 262144 2097152 16777216; do
 	echo -n "$i"
-	./eval_basic.sh with_pre_setup send 10.10.1.1 10.10.1.1 mlx5_2 ib_send_bw -s $i -r 64 -t 64 > tmp.data
+	./eval_basic.sh with_pre_setup send 10.10.1.1 10.10.1.1 mlx5_2 0 ib_send_bw -s $i -r 64 -t 64 > tmp.data
 	echo -n -e "\t`cat tmp.data | grep \"Wait-before-stop\" | awk '{print $2}'`"
 	sum=0.0
 	for key in DumpOthers Transfer FullRestore; do
